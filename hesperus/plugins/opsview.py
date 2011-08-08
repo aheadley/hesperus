3d88e7c73176f39372d4954fac6645a2b005d3b3
aheadley <aheadley@waysaboutstuff.com>
2011-08-08 16:16:18 -0400

    flesh out opsview plugin a bit

--- hesperus/plugins/opsview.py
+++ hesperus/plugins/opsview.py
@@ -1,4 +1,29 @@
 import opsview
+import ..plugin
+import time
 
 class OpsviewPlugin(CommandPlugin,PollPlugin):
-    pass
+    @plugin.Plugin.config_types(url=str, username=str, password=str, max_state_duration=int)
+    def __init__(self, core, url=None, username=None, password=None, max_state_duration=43200):
+        super(OpsviewPlugin, self).__init__(core)
+        if any(map(lambda x: x is None, [url, username, password])):
+            raise Exception('Missing setting')
+        else:
+            self.opsview_server = opsview.OpsviewServer(
+                base_url=url,
+                username=username,
+                password=password)
+            self.max_state_duration = max_state_duration
+            self.alerting = []
+
+    @plugin.CommandPlugin.register_command('status')
+    def status_command(self, chans, match, direct, reply):
+        pass
+
+    @plugin.CommandPlugin.register_command('ack')
+    def ack_command(self, chans, match, direct, reply):
+        pass
+
+    poll_interval = 15.0
+    def poll(self):
+        pass
